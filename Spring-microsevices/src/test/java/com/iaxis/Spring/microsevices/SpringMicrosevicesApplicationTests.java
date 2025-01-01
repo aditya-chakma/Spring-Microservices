c5530637a4d154f816025bce7d7cedd4bb3e2b21
Aditya Chakma <adityachakma199@gmail.com>
2025-01-01 20:28:20 +0600

    Initialized spring microservices tutorial

---
+++ Spring-microsevices/src/test/java/com/iaxis/Spring/microsevices/SpringMicrosevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iaxis.Spring.microsevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicrosevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
