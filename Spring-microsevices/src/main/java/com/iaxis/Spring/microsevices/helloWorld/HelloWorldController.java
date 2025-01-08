b57bc404fe844883a1fd31f6cf9d7643de48afe3
Aditya Chakma <aditya.chakma@therapservices.net>
2025-01-08 16:24:02 +0600

    Added swagger, Added jackson dataformat and implemented i18n

--- Spring-microsevices/src/main/java/com/iaxis/Spring/microsevices/helloWorld/HelloWorldController.java
+++ Spring-microsevices/src/main/java/com/iaxis/Spring/microsevices/helloWorld/HelloWorldController.java
@@ -1,9 +1,14 @@
 package com.iaxis.Spring.microsevices.helloWorld;
 
 import com.iaxis.Spring.microsevices.helloWorld.dto.HelloWorldDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author aditya.chakma
@@ -11,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -25,5 +37,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-dto/{name}")
     public HelloWorldDto helloWorldDto(@PathVariable String name) {
         return  new HelloWorldDto(String.format("Hello %s", name));
+    }
+
+    @GetMapping("/hello-world-i18n")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
